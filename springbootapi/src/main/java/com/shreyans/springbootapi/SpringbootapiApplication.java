351d9c8f3df1151e53301748717883631ba1a198
Shreyans Pathak <pathak.shreyans@gmail.com>
2022-01-26 01:23:22 -0500

    feat: :star: Added controller

--- springbootapi/src/main/java/com/shreyans/springbootapi/SpringbootapiApplication.java
+++ springbootapi/src/main/java/com/shreyans/springbootapi/SpringbootapiApplication.java
@@ -1,9 +1,13 @@
 package com.shreyans.springbootapi;
 
+import com.shreyans.springbootapi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +18,8 @@ public class SpringbootapiApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(new Student(1L, "Shreyans", "test@mail.com", LocalDate.of(1993, 12, 3), 21 ));
 	}
 
 }
