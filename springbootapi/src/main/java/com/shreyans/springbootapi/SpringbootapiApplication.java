7f8205a5cb545d51213f9921f2cc534dae8924dd
Shreyans Pathak <pathak.shreyans@gmail.com>
2022-01-25 20:19:31 -0500

    feat: :star: Added initial GET method

--- springbootapi/src/main/java/com/shreyans/springbootapi/SpringbootapiApplication.java
+++ springbootapi/src/main/java/com/shreyans/springbootapi/SpringbootapiApplication.java
@@ -2,12 +2,20 @@ package com.shreyans.springbootapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootapiApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
---
+++ springbootapi/src/main/java/com/shreyans/springbootapi/student/Student.java
@@ -0,0 +1,24 @@
+package com.shreyans.springbootapi.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+
+}
